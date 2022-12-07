23c42b13d59c839476a88914388832e381a006f1
MertColakk <115252588+MertColakk@users.noreply.github.com>
2022-12-07 21:02:55 +0300

    Update DiziyiTerstenYaz.c

--- DiziyiTerstenYaz.c
+++ DiziyiTerstenYaz.c
@@ -2,17 +2,12 @@
 
 int main()
 {
-    printf("Lutfen 5 adet sayı giriniz.\n");
-    int sayilar[5]={};
+    printf("Lutfen 5 adet sayı giriniz.\n"); //Kullanıcıya bilgi verilir.
+    int sayilar[5]={}; //Dizi tanımlama
     for (int i = 0; i < 5; i++)
-        scanf("%d",&sayilar[i]);
-    printf("Girdiğiniz sayıların tersi:");
-    for (int i = 4; i >= 0; i--)
+        scanf("%d",&sayilar[i]); //Döngü ile Dizi için kullanıcıdan sayı alınır
+    printf("Girdiğiniz sayıların tersi:"); //Kullanıcıya bilgi verilir.
+    for (int i = 4; i >= 0; i--) //Ters döngü ile alınan değerler geri yazılır.
         printf(" %d ",sayilar[i]);
-    
-    
-
-
-
     return 0;
 }
